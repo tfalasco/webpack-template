eb0e75c2db183cd6025037221a12826b31e486c4
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-03-09 07:44:53 -0400

    Add support for images
    Installed with command:
    npm install -D html-loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+
+// Example image import
+/*
+import imageName from "./images/imageName.svg";
+
+const image = document.createElement("img");
+image.src = imageName;
+document.body.appendChild(image);
+*/
