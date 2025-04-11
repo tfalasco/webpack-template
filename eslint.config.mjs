c8b7471f16fcca3a34c9e13c942866ff45dbffab
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-04-11 05:00:51 -0400

    Install eslint-config-prettier
    This helps eslint and prettier play nice together.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+	eslintConfigPrettier,
 ]);
